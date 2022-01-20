bf2948a3c761f81c1ae81d97f47ca00483e6747b
rpassmore01 <russellpassmore2005@gmail.com>
2022-01-20 13:36:31 -0500

    updated footer height, navbar button styling

--- components/navbar.js
+++ components/navbar.js
@@ -73,7 +73,7 @@ export default function Navbar() {
           handleBurger();
         }}
       >
-        <Image src={burgerIcon} width="32px" height="32px" alt=""></Image>
+        <Image src={burgerIcon} width="40px" height="40px" alt=""></Image>
       </button>
     </div>
   );
