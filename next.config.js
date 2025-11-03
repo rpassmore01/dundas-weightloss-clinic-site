65d23b2a16d775dc08edf60d1edc417de57d6425
MetallicGoat <ecazzam3@gmail.com>
2025-11-03 18:57:24 -0500

    Misc

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
-const nextEnv = require("next-env");
 const dotenvLoad = require("dotenv-load");
-
 dotenvLoad();
 
-const withNextEnv = nextEnv();
-
-module.exports = withNextEnv({
+module.exports = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
   },
-});
+};
